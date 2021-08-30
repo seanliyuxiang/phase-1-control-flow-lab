12a8d7c477241f88e2e6f17040ac7bd7b56e6536
Yuxiang Li <seanliyuxiang@gmail.com>
2021-08-30 16:41:00 -0500

    commit tree 6a235ec884b9be0c30d20876893c99e4425b6d59

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let greeting;
+
+  if (distance <= 400) {
+    greeting = 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    greeting = 'No can do.';
+  }
+
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let greeting;
+  
+  switch (tip) {
+    case "generous":
+      greeting = "Thank you so much.";
+      break;
+    case 'not as generous':
+      greeting = 'Thank you.';
+      break;
+    default:
+      greeting = 'Bye.';
+  }
+
+  return greeting;
 }
